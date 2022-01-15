8d3f65d2c4d6aa9bfa3fc74a8504fc9c3b0c267b
Lorenzo-Piersante <l.piersante@gmail.com>
2022-01-15 23:14:04 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from PIL import Image
+
+
+def generate_art():
+    base_image = Image.open(fp='images_to_compose/base.png')
+    resized_base_image = base_image.resize(size=(256, 256))
+
+    attachment_image = Image.open(fp='images_to_compose/attachment.png')
+    resized_attachment_image = attachment_image.resize(size=(64, 64))
+
+    mask = resized_attachment_image.point()
+    mask = resized_attachment_image.convert('L').point(lambda x: 0 if x > 128 else 255, '1')
+
+    resized_base_image.paste(im=resized_attachment_image, box=(92, 104), mask=mask)
+    resized_base_image.save(fp="generated_nft_set/test_image.png", format="PNG")
+
+    print("Art generated!")
+
+
+if __name__ == "__main__":
+    generate_art()
