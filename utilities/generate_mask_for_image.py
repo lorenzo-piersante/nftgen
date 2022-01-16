d5b982286d949701314771d59d4cd6f4026e9114
Lorenzo-Piersante <l.piersante@gmail.com>
2022-01-16 14:58:46 +0100

    add a comment to understand how pillow work

--- utilities/generate_mask_for_image.py
+++ utilities/generate_mask_for_image.py
@@ -6,8 +6,9 @@ def generate_mask_for_image(image: Image):
     data = image.getdata()
 
     new_image = []
-    for item in data:
-        if item == (255, 255, 255):
+    for pixel in data:
+        # pixel is represented from a tuple (r, g, b, a)
+        if pixel == (255, 255, 255):
             new_image.append((0, 0, 0))
             continue
         new_image.append((255, 255, 255))
