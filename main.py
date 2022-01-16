bd82382545eecd76a585771c0d97a39d78824f16
Lorenzo-Piersante <l.piersante@gmail.com>
2022-01-16 20:30:27 +0100

    refactor totale

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 from random import randint
 from PIL import Image
 from utilities.generate_mask_for_image import generate_mask_for_image
-from config import *
 
 
 def generate_art():
     x = 0
-    while x < 4:
+    while x < 6:
         background_id = randint(0, 2)
-        background = Image.open(fp='assets/background/' + str(background_id) + '.png')
-        new_image = background.resize(size=BACKGROUND_SIZE)
+        new_image = Image.open(fp='assets/background/' + str(background_id) + '.png')
 
-        cactus_id = randint(0, 2)
+        cactus_id = 0
         cactus = Image.open(fp='assets/cactus/' + str(cactus_id) + '.png')
-        resized_cactus = cactus.resize(size=CACTUS_SIZE)
-        cactus_mask = generate_mask_for_image(resized_cactus)
-        new_image.paste(im=resized_cactus, box=CACTUS_RIGHT_CORNER, mask=cactus_mask)
+        cactus_mask = generate_mask_for_image(cactus)
+        new_image.paste(im=cactus, box=(0, 0), mask=cactus_mask)
 
-        first_attachment_id = randint(0, 2)
+        first_attachment_id = randint(0, 1)
         first_attachment = Image.open(fp='assets/first_attachment/' + str(first_attachment_id) + '.png')
-        resized_first_attachment = first_attachment.resize(size=FIRST_ATTACHMENT_SIZE)
-        first_attachment_mask = generate_mask_for_image(resized_first_attachment)
-        new_image.paste(im=resized_first_attachment, box=FIRST_ATTACHMENT_RIGHT_CORNER, mask=first_attachment_mask)
+        first_attachment_mask = generate_mask_for_image(first_attachment)
+        new_image.paste(im=first_attachment, box=(0, 0), mask=first_attachment_mask)
 
-        second_attachment_id = randint(0, 2)
+        second_attachment_id = randint(0, 1)
         second_attachment = Image.open(fp='assets/second_attachment/' + str(second_attachment_id) + '.png')
-        resized_second_attachment = second_attachment.resize(size=SECOND_ATTACHMENT_SIZE)
-        second_attachment_mask = generate_mask_for_image(resized_second_attachment)
-        new_image.paste(im=resized_second_attachment, box=SECOND_ATTACHMENT_RIGHT_CORNER, mask=second_attachment_mask)
+        second_attachment_mask = generate_mask_for_image(second_attachment)
+        new_image.paste(im=second_attachment, box=(0, 0), mask=second_attachment_mask)
+
+        third_attachment_id = randint(0, 1)
+        third_attachment = Image.open(fp='assets/third_attachment/' + str(third_attachment_id) + '.png')
+        third_attachment_mask = generate_mask_for_image(third_attachment)
+        new_image.paste(im=third_attachment, box=(0, 0), mask=third_attachment_mask)
 
         new_image.save(fp='generated_nft_set/CryptoCactus#' + str(x) + '.png', format="PNG")
         x += 1
--- utilities/generate_mask_for_image.py
+++ utilities/generate_mask_for_image.py
@@ -2,13 +2,13 @@ from PIL import Image
 
 
 def generate_mask_for_image(image: Image):
-    image = image.convert("RGB")
+    image = image.convert("RGBA")
     data = image.getdata()
 
     new_image = []
     for pixel in data:
         # pixel is represented from a tuple (r, g, b, a)
-        if pixel == (255, 255, 255):
+        if pixel[3] == 0:
             new_image.append((0, 0, 0))
             continue
         new_image.append((255, 255, 255))
