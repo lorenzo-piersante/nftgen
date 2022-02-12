8176f6f126f0ecc285455002fee8d963dc781a6f
Lorenzo-Piersante <l.piersante@gmail.com>
2022-02-12 11:49:34 +0100

    doc

--- resize.py
+++ resize.py
@@ -2,14 +2,9 @@
 #
 # from PIL import Image
 #
-# i = 3
+# i = 0
 # while i <= 10:
 #     new_image = Image.open(fp='assets/background/' + str(i) + '.png').convert('RGBA')
 #     resized = new_image.resize((500, 500))
 #     resized.save(fp='assets/background/' + str(i) + '.png', format="PNG")
 #     i += 1
-#
-# i = 0
-# new_image = Image.open(fp='assets/body/' + str(i) + '.png').convert('RGBA')
-# resized = new_image.resize((500, 500))
-# resized.save(fp='assets/body/' + str(i) + '.png', format="PNG")
